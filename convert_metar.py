2182c6739c45a64abfbf3230fdd95a4407d79bfc
Dschubba <github@lesath.de>
2013-05-21 19:31:35 +0200

    Final first working version

--- convert_metar.py
+++ convert_metar.py
@@ -10,21 +10,21 @@ import profile, pstats
 import csv
 
 def usage():
-  program = os.path.basename(sys.argv[0])
-  print "Usage: ",program,"[-s] [<file>]"
-  print """Options:
-  <files> .. a set of files containing METAR reports to parse
-  -q ....... run "quietly" - just report parsing error.
-  -s ....... run silently. (no output)
-  -p ....... run with profiling turned on.
-  -f ....... the outputfile (default is ""output.csv"")
-  -j ....... write the output as a javascript array
-This program reads lines containing coded METAR reports from a file
-and prints human-reable reports.  Lines are taken from stdin if no
-file is given.  For testing purposes, the script can run silently,
-reporting only when a METAR report can't be fully parsed.
-"""
-  sys.exit(1)
+    program = os.path.basename(sys.argv[0])
+    print "Usage: ", program, "[-s] [<file>]"
+    print """Options:
+    <files> .. a set of files containing METAR reports to parse
+    -q ....... run "quietly" - just report parsing error.
+    -s ....... run silently. (no output)
+    -p ....... run with profiling turned on.
+    -f ....... the outputfile (default is ""output.csv"")
+    -j ....... write the output as a javascript array
+    This program reads lines containing coded METAR reports from a file
+    and prints human-reable reports.  Lines are taken from stdin if no
+    file is given.  For testing purposes, the script can run silently,
+    reporting only when a METAR report can't be fully parsed.
+    """
+    sys.exit(1)
 
 files = []
 written_values = set()
@@ -36,78 +36,82 @@ debug = False
 prof = False
 
 try:
-  opts, files = getopt.getopt(sys.argv[1:], 'dpqsjf')
-  for opt in opts:
-    if opt[0] == '-s':
-      silent = True
-      report = False
-    elif opt[0] == '-q':
-      report = False
-    elif opt[0] == '-d':
-      debug = True
-      Metar.debug = True
-    elif opt[0] == '-p':
-      prof = True
-    elif opt[0] == '-j':
-        outputfile = "output.js"
-        write_as_javascript = True
-    elif opt[0] == '-f':
-      outputfile = opt[1]
+    opts, files = getopt.getopt(sys.argv[1:], 'dpqsjf')
+    for opt in opts:
+        if opt[0] == '-s':
+            silent = True
+            report = False
+        elif opt[0] == '-q':
+            report = False
+        elif opt[0] == '-d':
+            debug = True
+            Metar.debug = True
+        elif opt[0] == '-p':
+            prof = True
+        elif opt[0] == '-j':
+            outputfile = "output.js"
+            write_as_javascript = True
+        elif opt[0] == '-f':
+            outputfile = opt[1]
 except:
-  usage()
+    usage()
 
 def output_to_csv(obs):
-  """Writes a single csv line to the output file."""
-  if obs.time not in written_values:
-      csvfile = open(outputfile,"a")
-      outputwriter = csv.writer(csvfile, delimiter=',', quotechar='"')
-      outputwriter.writerow([obs.time, obs.temp.value()])
-      written_values.add(obs.time)
-      csvfile.close()
+    """Writes a single csv line to the output file."""
+    if obs.time not in written_values:
+        if obs.temp:
+            csvfile = open(outputfile, "a")
+            outputwriter = csv.writer(csvfile, delimiter=',', quotechar='"')
+            outputwriter.writerow([obs.time, obs.temp.value()])
+            written_values.add(obs.time)
+            csvfile.close()
 
 def output_to_javascript(obs):
     """Writes a single javascript line to the output file."""
     if obs.time not in written_values:
-        javascriptfile = open(outputfile,"a")
-        javascriptfile.write("['{0}',{1:.1f}],".format(obs.time, obs.temp.value()))
-        written_values.add(obs.time)
-        javascriptfile.close()
+        if obs.temp:
+            javascriptfile = open(outputfile, "a")
+            javascriptfile.write("['{0}',{1:.1f}],".format(obs.time, obs.temp.value()))
+            written_values.add(obs.time)
+            javascriptfile.close()
 
 def make_valid_javascript():
     """Opens the written values and makes a valid Javascript array out of it."""
     if write_as_javascript:
-        javascriptfile = open(outputfile,"r")
+        javascriptfile = open(outputfile, "r")
         contents = javascriptfile.read()
         javascriptfile.close()
-        javascriptcomplete = "var cosPoints = [{0}];".format(contents[:-1])
-        javascriptfile = open(outputfile,"w")
+        javascriptcomplete = "var temperaturePoints = [{0}];".format(contents[:-1])
+        javascriptfile = open(outputfile, "w")
         javascriptfile.write(javascriptcomplete)
         javascriptfile.close()
 
 
 def process_line(line):
-  """Decode a single input line."""
-  line = line.strip()
-  if len(line) and line[0] in string.uppercase:
-    try:
-      obs = Metar.Metar(line)
-      if report:
-        print "--------------------"
-        print obs.string()
-        if write_as_javascript:
-           output_to_javascript(obs)
-        else:
-           output_to_csv(obs)
-    except Metar.ParserError, err:
-      if not silent:
-        print "--------------------"
-        print "METAR code: ",line
-        print string.join(err.args,", ")
+    """Decode a single input line."""
+    line = line.strip()
+    if len(line) and line[0] in string.uppercase:
+        try:
+            obs = Metar.Metar(line)
+            if report:
+                print "--------------------"
+                print obs.string()
+                if write_as_javascript:
+                    output_to_javascript(obs)
+                else:
+                    output_to_csv(obs)
+        except Metar.ParserError, err:
+            if not silent:
+                print "--------------------"
+                print "METAR code: ", line
+                print string.join(err.args, ", ")
 
 def process_files(files):
   """Decode METAR lines from the given files."""
+  if os.path.isfile(outputfile):
+     os.remove(outputfile)
   for file in files:
-    fh = open(file,"r")
+    fh = open(file, "r")
     for line in fh.readlines():
       process_line(line)
     fh.close()
