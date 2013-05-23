875b16d7bee9ec36e0b996ceb653d7cfe18a4816
Wolfram Sobotta <bitbucket@lesath.de>
2013-05-23 14:36:13 +0200

    Made the script more intelligent, it only fetches the data if changed, plus some OS type decisions

--- geteddm.sh
+++ geteddm.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-wget http://weather.noaa.gov/pub/data/observations/metar/stations/EDDM.TXT -O $HOME/METAR/EDDM-`date  "+%Y%m%d%H%M"`.txt -a $HOME/METAR/logfile.txt
+
+PLATFORM=`uname -s`
+WGET="wget"
+if [[ "$PLATFORM" == "Darwin" ]]; then
+	WGET="/sw/bin/wget"
+fi
+
+HOME_DIRECTORY="$HOME/METAR/"
+
+ACTUAL_FILE="EDDM-actual.txt"
+DOWNLOADED_FILE="EDDM.TXT"
+TARGET_FILE="EDDM-`date "+%Y%m%d%H%M"`.txt"
+
+cd $HOME_DIRECTORY
+
+$WGET -N http://weather.noaa.gov/pub/data/observations/metar/stations/EDDM.TXT -a logfile.txt 
+
+if [ "$ACTUAL_FILE" -ot "$DOWNLOADED_FILE" ]; then
+	cp -a $DOWNLOADED_FILE $ACTUAL_FILE
+	cp -a $ACTUAL_FILE $TARGET_FILE
+fi
