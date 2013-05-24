9714f0d47ae4f8cc14bfc8f067905f296683e311
Wolfram Sobotta <bitbucket@lesath.de>
2013-05-24 10:39:32 +0200

    Stored the different files in directories for that day

--- geteddm.sh
+++ geteddm.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
+# On which platform do we run? We decide to use which wget later...
 PLATFORM=`uname -s`
 WGET="wget"
+WGET_LOGFILE="logfile.txt"
+
+# Where to store the METAR-Data
+HOME_DIRECTORY="$HOME/METAR/"
+
+# Holds the file which is last stored - this is 
+ACTUAL_FILE="EDDM-actual.txt"
+# compared to this file
+DOWNLOADED_FILE="EDDM.TXT"
+# This is where we store the files for that day
+TARGET_DIRECTORY="$HOME_DIRECTORY/`date "+%Y"`/`date "+%m"`/`date "+%d"`"
+# The filename of the new file - actually this is copied from the "ACTUAL_FILE"
+TARGET_FILE="EDDM-`date "+%Y%m%d%H%M"`.txt"
+
 if [[ "$PLATFORM" == "Darwin" ]]; then
 	WGET="/sw/bin/wget"
 fi
 
-HOME_DIRECTORY="$HOME/METAR/"
-
-ACTUAL_FILE="EDDM-actual.txt"
-DOWNLOADED_FILE="EDDM.TXT"
-TARGET_FILE="EDDM-`date "+%Y%m%d%H%M"`.txt"
-
 cd $HOME_DIRECTORY
+mkdir -p $TARGET_DIRECTORY
 
-$WGET -N http://weather.noaa.gov/pub/data/observations/metar/stations/EDDM.TXT -a logfile.txt 
+$WGET -N http://weather.noaa.gov/pub/data/observations/metar/stations/EDDM.TXT -a $WGET_LOGFILE
 
 if [ "$ACTUAL_FILE" -ot "$DOWNLOADED_FILE" ]; then
 	cp -a $DOWNLOADED_FILE $ACTUAL_FILE
-	cp -a $ACTUAL_FILE $TARGET_FILE
+	cp -a $ACTUAL_FILE "$TARGET_DIRECTORY/$TARGET_FILE"
 fi
+
